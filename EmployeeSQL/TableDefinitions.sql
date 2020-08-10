12291ad4a8fc07686c4061fe03b35e682d0c9300
Douglas High <douglashigh17@gmail.com>
2020-08-10 15:48:53 -0400

    getting close

--- EmployeeSQL/TableDefinitions.sql
+++ EmployeeSQL/TableDefinitions.sql
@@ -1,12 +1,13 @@
 /**************************************************************
-*  RUT-SOM-DATA-PT-06-2020-U-C                    Douglas High*
+*  RUT-SOM-DATA-PT-06-2020-U-C                   Douglas High *
 *   >SQL-Challenge                            August 10, 2020 *
 *                    TABLE DEFINITION SQL                     *
 ***************************************************************
 */
--- ** uncomment below lIne to drop all table  **--
---DROP TABLE Titles, Employees, Departments, Dept_emp, Dept_manager, Salaries;
+-- **  drop all tables  **--
+DROP TABLE IF EXISTS Titles, Employees, Departments, Dept_emp, Dept_manager, Salaries;
 
+-- **  create tables  **--
 CREATE TABLE Titles (
     "title_id" char(5) NOT NULL PRIMARY KEY,
     "title" varchar(30) NOT NULL
@@ -28,16 +29,18 @@ CREATE TABLE Departments (
 	);
 
 CREATE TABLE Dept_emp (
-    "emp_no" int NOT NULL REFERENCES Employees ("emp_no"),
-    "dept_no" char(5) NOT NULL REFERENCES Departments ("dept_no")
+	"emp_no" int NOT NULL REFERENCES Employees ("emp_no"),
+	"dept_no" char(5) NOT NULL REFERENCES Departments ("dept_no"),
+	CONSTRAINT "pk_Dept_emp" PRIMARY KEY ("emp_no","dept_no")	
 	);
 
 CREATE TABLE Dept_manager (
     "dept_no" char(4) NOT NULL,
-    "emp_no" int NOT NULL
+    "emp_no" int NOT NULL,
+	CONSTRAINT "pk_Dept_manager" PRIMARY KEY ("dept_no","emp_no")	
 	);
 
 CREATE TABLE Salaries (
-    "emp_no" int NOT NULL,
+    "emp_no" int NOT NULL PRIMARY KEY,
     "salary" int NOT NULL
 	);
